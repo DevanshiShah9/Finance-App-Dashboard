40655f70ac3e480c0e27172496c411956d5c0754
Devanshi Shah <shahdeva@usc.edu>
2024-02-25 14:17:11 -0800

    mui configurations and theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,16 @@
+import { useMemo } from "react"
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from "./theme"
+import { ThemeProvider, CssBaseline } from "@mui/material"
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        hi
+      </ThemeProvider>
     </div>
   )
 }
